53666060fa10773878725a9a7d62dc468eb4ed31
129Ashish <kumarashisha54@gmail.com>
2025-01-30 16:38:51 +0530

    changed full

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Add interactivity here
+document.querySelector('.cta-button').addEventListener('click', () => {
+    alert('Welcome to JavaScript Universe!');
+  });
+  console.log("hello world");
+  
